6225ff8b3b359bc0bc14cabe2ae1b02e94df4215
drip016 <73792907+drip016@users.noreply.github.com>
2022-04-29 10:16:28 +0200

    style: Style adjustments for several components

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -1,16 +1,17 @@
 import './CountryCard.scss';
+import NumberFormat from 'react-number-format';
 const CountryCard = ({ countryInfo }) => {
   return (
     <div className="country-card">
       <div className="country-card__image">
-        <img src={countryInfo.flag} alt="" />
+        <img src={countryInfo.flags.png} alt="" />
       </div>
       <div className="country-card__info">
         <h4>{countryInfo.name}</h4>
         <ul>
           <li>
             <span>Population: </span>
-            {countryInfo.population}
+            {parseInt(countryInfo.population).toLocaleString()}
           </li>
           <li>
             <span>Region: </span>
